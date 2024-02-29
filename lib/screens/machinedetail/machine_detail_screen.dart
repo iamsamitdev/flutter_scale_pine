98f3687e4acacd44795ec2a116f13a55e94f66d3
Samit Koyom <samitkoyom@gmail.com>
2024-02-29 15:03:04 +0700

    update model and service7

--- lib/screens/machinedetail/machine_detail_screen.dart
+++ lib/screens/machinedetail/machine_detail_screen.dart
@@ -11,20 +11,35 @@ class _MachineDetailScreenState extends State<MachineDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Machine 1'),
-        ),
-        body: ListView(
-          children: const [
-            SizedBox(height: 10),
-            Text('Machine 1',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            SizedBox(height: 10),
-            Text('Location: ตึก A ชั้น 2',
-                textAlign: TextAlign.center, style: TextStyle(fontSize: 16)),
-          ],
-        ),
-        );
+      appBar: AppBar(
+        title: const Text('Machine 1'),
+      ),
+      body: ListView(
+        children: [
+          Image.network(
+            'https://www.itgenius.co.th/sandbox_api/plcapi/images/machines/machine-1.jpg',
+            height: 400.0,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 10),
+          const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Machine ID: '),
+                      Text(
+                        '1',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
+              ))
+        ],
+      ),
+    );
   }
 }
