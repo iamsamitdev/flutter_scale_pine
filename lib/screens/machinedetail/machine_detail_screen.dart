2819cc2095ef802bc0e730a8469c76717ac9d850
Samit Koyom <samitkoyom@gmail.com>
2024-02-29 15:18:48 +0700

    update model and service8

--- lib/screens/machinedetail/machine_detail_screen.dart
+++ lib/screens/machinedetail/machine_detail_screen.dart
@@ -18,26 +18,113 @@ class _MachineDetailScreenState extends State<MachineDetailScreen> {
         children: [
           Image.network(
             'https://www.itgenius.co.th/sandbox_api/plcapi/images/machines/machine-1.jpg',
-            height: 400.0,
+            height: 300.0,
             fit: BoxFit.cover,
           ),
           const SizedBox(height: 10),
           const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Machine ID: '),
-                      Text(
-                        '1',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                ],
-              ))
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('ID: '),
+                    Text(
+                      '1',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Name: '),
+                    Text(
+                      'Machine 1',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Location: '),
+                    Text(
+                      'Factory 1',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Status: '),
+                    Text(
+                      'ทำงานปกติ',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Maintenence: '),
+                    Text(
+                      'ไม่ต้องซ่อมบำรุง',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('รุ่น: '),
+                    Text(
+                      'XYZ-123',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('ผู้ผลิต: '),
+                    Text(
+                      'ABC Company',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('ปีที่ผลิต: '),
+                    Text(
+                      '2020',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Daily check: '),
+
+                  ],
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
